d210de006c6ec8289f487bbf7d774ed6ea3d0ba2
Shoshana Aharon <shoshaha@Shoshanas-Air-2.fios-router.home>
2025-03-04 14:47:40 -0500

    Sudoku updated

---
+++ src/main/java/aharon/sudoku/Sudoku.java
@@ -0,0 +1,56 @@
+package aharon.sudoku;
+
+import java.util.*;
+
+public class Sudoku {
+
+    private int[][] board;
+
+    public Sudoku (int[][] board) {
+        this.board = board;
+    }
+
+    public List<String> getErrors()
+    {
+        List<String> errors = new ArrayList<>();
+
+        for (int col = 0; col < 9; col++) {
+            int[] numsPassed = new int[9];
+            int countPassed = 0;
+
+            for (int row = 0; row < 9; row++) {
+                int currentNumber = board[row][col];
+
+                for (int np = 0; np < countPassed; np++) {
+                    if (numsPassed[np] == currentNumber) {
+                        String error = "Duplicate of " + currentNumber + " in row " + row + ", column " + col;
+                        errors.add(error);
+                        break;
+                    }
+                }
+                numsPassed[countPassed] = currentNumber;
+                countPassed++;
+            }
+        }
+
+        for (int row = 0; row < 9; row++) {
+            int[] numsPassed = new int[9];
+            int countPassed = 0;
+
+            for (int col = 0; col < 9; col++) {
+                int currentNumber = board[row][col];
+
+                for (int np = 0; np < countPassed; np++) {
+                    if (numsPassed[np] == currentNumber) {
+                        String error = "Duplicate of " + currentNumber + " in row " + row + ", column " + col;
+                        errors.add(error);
+                        break;
+                    }
+                }
+                numsPassed[countPassed] = currentNumber;
+                countPassed++;
+            }
+        }
+        return errors;
+    }
+}
