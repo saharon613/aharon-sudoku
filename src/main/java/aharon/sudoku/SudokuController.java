884e96ca2920a0fdb0b6459ed9da61c305107d80
Shoshana Aharon <shoshaha@Shoshanas-Air-2.fios-router.home>
2025-03-18 11:44:20 -0400

    updated

--- src/main/java/aharon/sudoku/SudokuController.java
+++ src/main/java/aharon/sudoku/SudokuController.java
@@ -1,5 +1,7 @@
 package aharon.sudoku;
 
+import javax.swing.*;
+
 public class SudokuController {
     private final Sudoku sudoku;
     private final SudokuFrame frame;
@@ -7,5 +9,23 @@ public class SudokuController {
     public SudokuController(Sudoku sudoku, SudokuFrame frame) {
         this.sudoku = sudoku;
         this.frame = frame;
+    }
+
+    public void readGUI(){
+        JTextField[][] cells = frame.textFields;
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                String cellText = cells[row][col].getText();
+                if (!cellText.isEmpty()) {
+                    try {
+                        int value = Integer.parseInt(cellText);
+                        sudoku.board[row][col] = value;
+                    }
+                    catch (NumberFormatException e) {
+                        sudoku.board[row][col] = 0;
+                    }
+                }
+            }
+        }
     }
 }
